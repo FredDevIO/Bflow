3847265642cae9845fc0a47156462fdf6e0ccce5
Fred <federicojames219@gmail.com>
2026-03-05 17:50:38 +0100

    init: project initialization

---
+++ src/main/java/io/github/lucyfred/bflow/BflowApplication.java
@@ -0,0 +1,13 @@
+package io.github.lucyfred.bflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/lucyfred/bflow/BflowApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucyfred.bflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
