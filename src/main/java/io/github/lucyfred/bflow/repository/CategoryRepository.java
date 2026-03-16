a135f41bd9f7072665623b43735bbb126aa7df08
fred <federicojames219@gmail.com>
2026-03-16 12:33:18 +0100

    refactor: improve data types and add validation constraints - Update data types for better precision - Change @Size message - Define missing methods in repository interfaces

--- src/main/java/io/github/lucyfred/bflow/repository/CategoryRepository.java
+++ src/main/java/io/github/lucyfred/bflow/repository/CategoryRepository.java
@@ -2,11 +2,12 @@ package io.github.lucyfred.bflow.repository;
 
 import io.github.lucyfred.bflow.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> getAllByUserId(Long userId);
+    Optional<Category> findByIdAndUserId(Long id, Long userId);
     boolean existsByNameAndUserId(String name, Long userId);
     boolean existsByIdAndUserId(Long id, Long userId);
 }
